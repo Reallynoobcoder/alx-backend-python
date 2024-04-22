d50e5cf9fe616b3ea87f1bbb02dc45ee37da2b43
El imali hamza <elimal.hamza@gmail.com>
2024-04-22 14:47:21 +0000

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,6 +4,9 @@ from time import time
 """Function that the average time."""
 
 
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
 def measure_time(n: int, max_delay: int) -> float:
     """Returns average time"""
     a = time()
