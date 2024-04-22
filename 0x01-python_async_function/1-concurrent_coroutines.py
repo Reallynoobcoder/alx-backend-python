4456776a7ba37cc8cdf88ff953d0ca5ec2c36166
El imali hamza <elimal.hamza@gmail.com>
2024-04-22 13:47:55 +0000

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Async routine."""
+
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Wait for n random delays."""
+    new_list: List[float] = []
+
+    for _ in range(n):
+        if len(new_list) >= n:
+            break
+        t = await wait_random(max_delay)
+        new_list.append(t)
+
+    return sorted(new_list)
