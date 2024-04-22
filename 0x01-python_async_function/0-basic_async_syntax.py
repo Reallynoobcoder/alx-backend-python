43069fa0308e5ba7955539eddb43b4d0b136a31f
El imali hamza <elimal.hamza@gmail.com>
2024-04-22 11:12:26 +0000

    requirement

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from random import uniform, randint
+from random import uniform
 import asyncio
 """
 Synchronous coroutine that takes in an integer argument
@@ -9,5 +9,6 @@ max_delay, and return a random delay between 0 and max_delay.
 
 async def wait_random(max_delay: int = 10) -> float:
     """Return random delay between 0 and max_delay"""
-    await asyncio.sleep(randint(0, max_delay))
-    return uniform(0, max_delay)
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
