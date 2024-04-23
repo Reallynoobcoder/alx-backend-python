77da4c05565c7de0e92a283f6ed3c6132d372542
El imali hamza <elimal.hamza@gmail.com>
2024-04-23 17:58:37 +0000

    .

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,4 +14,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Loop 10 for each second."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
