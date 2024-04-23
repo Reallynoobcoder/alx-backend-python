647199e9b0bb96bf41acafb4c7711e688d6e5a31
El imali hamza <elimal.hamza@gmail.com>
2024-04-23 17:47:44 +0000

    .

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Coroutine that collect 10 random numbers."""
-
-
+import asyncio
+import random
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Return the 10 random numbers"""
-    results = [i async for i in async_generator()]
-    return results
+    """Return 10 random numbers."""
+    return [i async for i in async_generator()]
