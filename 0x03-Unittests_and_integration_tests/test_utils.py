d2c63c24b19622e04f9400c7e8fdfa7a49cd6dc6
El imali hamza <elimal.hamza@gmail.com>
2024-05-10 12:10:32 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
-        self, nested_map: Dict[str, Any], path: Tuple[str], expected: Any
+        self, nested_map: Dict[str, Any], path: Tuple[str],
+        expected_result: Any
     ) -> None:
         """Unit test for utils.access_nested_map."""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
