2b48f503bd962686f8827fec076357fb60abc414
El imali hamza <elimal.hamza@gmail.com>
2024-05-12 14:36:44 +0000

    .

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test."""
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized, parameterized_class
+import unittest
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Defines a test case class for testing GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test org method of GithubOrgClient"""
+        client = GithubOrgClient(org_name)
+
+        mock_get_json.return_value = {
+            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
+
+        result = client.org
+
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+
+        self.assertEqual(
+            result, {
+                "repos_url": f"https://api.github.com/orgs/{org_name}/repos"})
